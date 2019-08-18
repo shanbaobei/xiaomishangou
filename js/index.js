01bae8eed03116d53e73f80bd74914749c633afa
shanbaobei <812858143@qq.com>
2019-08-18 22:50:34 +0800

    1.1调整固定导航栏

--- js/index.js
+++ js/index.js
@@ -29,10 +29,10 @@ window.onscroll = function(){
     var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop || 0;
 
     if(scrollTop >=260){
-        seckillNav.className = "seckill-nav seckill-navfixed";
+        seckillNav.className = "container seckill-nav seckill-navfixed";
 
     }else {
-        seckillNav.className = "eckill-nav";
+        seckillNav.className = "container seckill-nav";
     }
     console.log(scrollTop);
 }
